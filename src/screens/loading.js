7e962afa1dffc15a5c460b14794369b85a8987cc
Sam <samong0105@gmail.com>
2019-08-31 15:34:21 +1200

    Redirect user to home or signup page depending on auth status

--- src/screens/loading.js
+++ src/screens/loading.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import firebase from 'react-native-firebase';
 
 export default class Loading extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Home' : 'SignUp')
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}><Text style={{color:'#e93766', fontSize: 40}}>Loading</Text><ActivityIndicator color='#e93766' size="large" /></View>
