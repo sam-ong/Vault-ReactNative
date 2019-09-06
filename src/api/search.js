c74de9f86945fa447703e049e9d9403eb2ed8994
Sam <samong0105@gmail.com>
2019-09-06 12:51:49 +1200

    Add search function, work in progress

---
+++ src/api/search.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { getSearchShowsUrl } from '../api/urls';
+
+// ------------------------------------------------------
+// Show details
+// ------------------------------------------------------
+
+export const fetchSearchResults = ({ query }, reqParams = {}) =>
+  new Promise(async (resolve, reject) => {
+    const url = getSearchShowsUrl({ query: query });
+
+    try {
+      const { data } = await axios.get(url, reqParams);
+      debugger;
+      resolve(data);
+    } catch (error) {
+      Config.logNetworkErrors && console.log(error);
+      reject(error);
+    }
+  });
--- src/screens/search.js
+++ src/screens/search.js
@@ -1,22 +1,44 @@
 import React from 'react'
-import { StyleSheet, Platform, Image, Text, View, Button } from 'react-native'
+import { Platform, Image, Text, View, Button, TextInput, FlatList } from 'react-native'
+import { fetchSearchResults } from '../api/search'
+import styles from './style'
 import firebase from 'react-native-firebase';
 
 export default class Search extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            query: '',
+            results: []
+        }
+        this.handleSearch = this.handleSearch.bind(this);
+    }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text> SEARCH </Text>
-      </View>
-    )
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text> SEARCH </Text>
+                <TextInput
+                    style={styles.textInput}
+                    autoCapitalize="none"
+                    placeholder="Search for a title..."
+                    onChangeText={query => this.setState({ query })}
+                    value={this.state.query}
+                />
+                <Button title="Search" color="#e93766" onPress={this.handleSearch} />
+
+                <FlatList
+                    data = {this.state.results}
+                    renderItem = { ({show}) => <Text>{show.title}</Text> }
+                />
+
+            </View>
+        )
+    }
+
+    handleSearch = () => {
+        let query = this.state.query
+        this.setState({ results: fetchSearchResults({query}).results })
+        debugger;
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
